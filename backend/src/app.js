d768187d262b7cd526d1569e8f9da5590309ea99
Lakshya Jain <lakshyajain013@gmail.com>
2026-06-27 13:07:46 +0530

    Setting up Backend Structure

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+import cors from "cors";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 8000);
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+const start = async () => {
+  const connectionDB = await mongoose.connect(
+    "mongodb+srv://lakshyajain013:KonvoDB@konvo.vwbb8xr.mongodb.net/",
+  );
+
+  console.log(`MONGO Connected DB Host : ${connectionDB.connection.host}`);
+  server.listen(app.get("port"), () => {
+    console.log("Listining on port 8000");
+  });
+};
+
+start();
