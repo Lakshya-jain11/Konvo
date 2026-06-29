f3bfbf05bdc13d4820bc9d2578f4d9c369c0d6f0
Lakshya Jain <lakshyajain013@gmail.com>
2026-06-29 23:44:40 +0530

    Created Landing Page

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function landing() {
+    return (
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>Konvo</h2>
+                </div>
+                <div className='navlist'>
+                    <p>Join as Guest</p>
+                    <p>Register</p>
+                    <div role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect </span>with your Loved Ones</h1>
+                    <p>Cover a distance by Konvo</p>
+                    <div role='button'>
+                        <Link to={"/home"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+                    <img src="/mobile.png" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
